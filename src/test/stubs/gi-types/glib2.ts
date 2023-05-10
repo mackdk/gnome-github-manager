f88928493bc77b0b9ad8c25d1184e46258af8067
Thomas Florio <mackdk@hotmail.com>
2023-05-10 02:38:41 +0200

    Added unit test for LimitedRetriableTimer

--- src/test/stubs/gi-types/glib2.ts
+++ src/test/stubs/gi-types/glib2.ts
@@ -3,3 +3,28 @@ export function free(mem?: any | null): void {}
 export function get_user_data_dir(): string {
     return '/user/data/dir';
 }
+
+const timersMap = new Map<number, NodeJS.Timeout>();
+let index: number = 0;
+
+export const PRIORITY_DEFAULT = 0;
+
+export function timeout_add_seconds(_priority: number, interval: number, task: () => boolean): number {
+    const currentIndex = index++;
+    const timeout = setTimeout(() => {
+        task();
+        timersMap.delete(currentIndex);
+    }, interval * 1000);
+    timersMap.set(currentIndex, timeout);
+    return currentIndex;
+}
+
+export function source_remove(handle: number): boolean {
+    const timer = timersMap.get(handle);
+    if (timer !== undefined) {
+        clearTimeout(timer);
+        timersMap.delete(handle);
+    }
+
+    return false;
+}
